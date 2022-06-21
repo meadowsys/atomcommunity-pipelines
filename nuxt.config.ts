5937aa7f4478ad961f91657b35de8de3e33c68df
Autumn Meadow <blazeykirin@gmail.com>
2022-06-21 13:51:00 -0700

    more init
    - delete nuxt telemetry
    - nuxt configuration
    - install tailwind and configure
    - install zod from source because i dunno why but i like to do it from source

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,41 @@
 import { defineNuxtConfig } from "nuxt";
+import path from "path";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	telemetry: false
+	telemetry: false,
+	target: "static",
+	ssr: false,
+	app: {
+		baseURL: "/atomcommunity-pipelines/"
+	},
+	alias: {
+		zod: path.resolve("./node_modules/_zod/src/index.ts")
+	},
+	vite: {
+		build: {
+			minify: "terser",
+			terserOptions: {
+				compress: {
+					passes: 3
+				}
+			}
+		}
+	},
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {}
+		}
+	},
+	typescript: {
+		shim: false,
+		strict: true,
+		typeCheck: "build",
+		tsConfig: {
+			compilerOptions: {
+				skipLibCheck: false
+			}
+		}
+	}
 });
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./app.vue",
+		"./{assets,components,composables,layouts,pages}/**/*.{vue,ts,js,tsx,jsx}"
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
